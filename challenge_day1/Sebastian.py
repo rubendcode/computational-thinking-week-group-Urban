79d6e4feb17850533368233d505aaff44e28063a
Sebastian Graeff <41366273+sebastian-graeff@users.noreply.github.com>
2023-09-04 13:47:42 +0200

    Update Sebastian.py

--- challenge_day1/Sebastian.py
+++ challenge_day1/Sebastian.py
@@ -1,2 +1,10 @@
 def Sebastian():
     return "Sebastian"
+
+sebastian_character = "Luna"
+
+sebastian_paragraphs = [
+    "Luna felt a shiver run down her spine, seeing the map. Beside her, Marcus, Elara, Orion, and Draco pondered its origins.",
+    "Under the moonlight, Luna played a tune on her flute, mesmerizing Marcus, Elara, Orion, and Draco.",
+    "With the dawn's first light, Luna guided Marcus, Elara, Orion, and Draco through the dense forest."
+]
