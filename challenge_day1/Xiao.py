94f256d6faf35bed9177d34dc3947df4f020acb6
Quanpu Xiao <aoyamaxx@re-byods-146-50-199-238.wireless.uva.nl>
2023-09-04 13:49:19 +0200

    Xiao's story

--- challenge_day1/Xiao.py
+++ challenge_day1/Xiao.py
@@ -1,2 +1,10 @@
 def Xiao():
     return "Xiao"
+
+xiao_character = "Marcus"
+
+xiao_paragraphs = [
+    "Marcus, with Luna, Elara, Orion, and Draco, found an old map in the attic.",
+    "That night, Marcus listened as Luna and Elara recounted tales of the sea, while Orion and Draco debated their next move.",
+    "Marcus, leading Luna, Elara, Orion, and Draco, embarked on a treasure hunt the following morning."
+]
--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,8 +1,8 @@
-from Xiao import Xiao
-from Sebastian import Sebastian
-from ruben import Ruben
-from sofia import Sofia
-from Kelt import Kelt
+from Xiao import Xiao, xiao_paragraphs
+from Sebastian import Sebastian, sebastian_paragraphs
+from ruben import Ruben, kelt_paragraphs
+from sofia import Sofia, ruben_paragraphs
+from Kelt import Kelt, sofia_paragraphs
 
 def naming():
     members = [Xiao(), Sebastian(), Ruben(), Sofia(), Kelt()]
@@ -10,3 +10,15 @@ def naming():
     return f"This is Team Urban.\nWe are:\n{members_str}"
 
 print(naming())
+
+def print_story():
+
+    for act in range(3):
+        print(xiao_paragraphs[act])
+        print(sebastian_paragraphs[act])
+        print(kelt_paragraphs[act])
+        print(ruben_paragraphs[act])
+        print(sofia_paragraphs[act])
+
+if __name__ == "__main__":
+    print_story()
