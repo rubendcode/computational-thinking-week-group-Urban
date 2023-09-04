e12683b3d4d4f0017ecf8d8c6ca499c42dfbab23
Quanpu Xiao <aoyamaxx@re-byods-146-50-199-238.wireless.uva.nl>
2023-09-04 13:33:47 +0200

    Sofia's name

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,9 +1,10 @@
 from Xiao import Xiao
 from Sebastian import Sebastian
 from ruben import Ruben
+from sofia import Sofia
 
 def naming():
-    members = [Xiao(), Sebastian(), Ruben()]
+    members = [Xiao(), Sebastian(), Ruben(), Sofia()]
     members_str = "\n".join(members)
     return f"This is Team Urban.\nWe are:\n{members_str}"
 
