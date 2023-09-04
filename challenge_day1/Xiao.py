b18dfda9fa62b1e10d836a59c30d1a18a4503132
Quanpu Xiao <aoyamaxx@re-byods-146-50-199-238.wireless.uva.nl>
2023-09-04 14:36:01 +0200

    Xiao file change

--- challenge_day1/Xiao.py
+++ challenge_day1/Xiao.py
@@ -1,10 +1,15 @@
+from Sebastian import sebastian_character
+from ruben import Ruben, ruben_character
+from sofia import Sofia, sofia_character
+from Kelt import Kelt, kelt_character
+
 def Xiao():
     return "Xiao"
 
 xiao_character = "Marcus"
 
 xiao_paragraphs = [
-    f"{xiao_character}, with Luna, Elara, Orion, and Draco, found an old map in the attic.",
-    f"That night, {xiao_character} listened as Luna and, Elara recounted tales of the sea, while Orion and Draco debated their next move.",
-    f"{xiao_character}, leading Luna, Elara, Orion, and Draco, embarked on a treasure hunt the following morning."
+    f"{xiao_character}, with {sebastian_character}, {kelt_character}, {ruben_character}, and {sofia_character}, found an old map in the attic.",
+    f"That night, {xiao_character} lisdened as {sebastian_character} and, {kelt_character} recounted tales of the sea, while {ruben_character} and {sofia_character} debated their next move.",
+    f"{xiao_character}, leadding {sebastian_character}, {kelt_character}, {ruben_character}, and {sofia_character}, embarked on a treasure hunt the following murning."
 ]
