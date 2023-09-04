73a533eb8bde6546ae2b1a7352021c9a4dea061d
Quanpu Xiao <aoyamaxx@re-byods-146-50-199-238.wireless.uva.nl>
2023-09-04 14:29:46 +0200

    Xiao update

--- challenge_day1/Xiao.py
+++ challenge_day1/Xiao.py
@@ -4,7 +4,7 @@ def Xiao():
 xiao_character = "Marcus"
 
 xiao_paragraphs = [
-    "Marcus, with Luna, Elara, Orion, and Draco, found an old map in the attic.",
-    "That night, Marcus listened as Luna and Elara recounted tales of the sea, while Orion and Draco debated their next move.",
-    "Marcus, leading Luna, Elara, Orion, and Draco, embarked on a treasure hunt the following morning."
+    f"{xiao_character}, with Luna, Elara, Orion, and Draco, found an old map in the attic.",
+    f"That night, {xiao_character} listened as Luna and, Elara recounted tales of the sea, while Orion and Draco debated their next move.",
+    f"{xiao_character}, leading Luna, Elara, Orion, and Draco, embarked on a treasure hunt the following morning."
 ]
