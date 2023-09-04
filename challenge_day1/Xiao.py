0d0b8136c01d9f9c2ba97039f9f9ebafa4432029
rubendcode <128597405+rubendcode@users.noreply.github.com>
2023-09-04 14:45:22 +0200

    Update Xiao.py

--- challenge_day1/Xiao.py
+++ challenge_day1/Xiao.py
@@ -10,6 +10,6 @@ xiao_character = "Marcus"
 
 xiao_paragraphs = [
     f"{xiao_character}, with {sebastian_character}, {kelt_character}, {ruben_character}, and {sofia_character}, found an old map in the attic.",
-    f"That night, {xiao_character} lisdened as {sebastian_character} and, {kelt_character} recounted tales of the sea, while {ruben_character} and {sofia_character} debated their next move.",
-    f"{xiao_character}, leadding {sebastian_character}, {kelt_character}, {ruben_character}, and {sofia_character}, embarked on a treasure hunt the following murning."
+    f"That night, {xiao_character} listened as {sebastian_character} and, {kelt_character} recounted tales of the sea, while {ruben_character} and {sofia_character} debated their next move.",
+    f"{xiao_character}, leading {sebastian_character}, {kelt_character}, {ruben_character}, and {sofia_character}, embarked on a treasure hunt the following morning."
 ]
