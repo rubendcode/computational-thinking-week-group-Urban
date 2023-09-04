e03d5573f84e1eae4b00ec0cc2a28cb55ac7b9ee
rubendcode <128597405+rubendcode@users.noreply.github.com>
2023-09-04 14:35:57 +0200

    Update ruben.py

--- challenge_day1/ruben.py
+++ challenge_day1/ruben.py
@@ -1,10 +1,16 @@
+
+from Sebastian import sebastian_character
+from ruben import Ruben, ruben_character
+from sofia import Sofia, sofia_character
+from Kelt import Kelt, kelt_character
+from Xiao import Xiao, xiao_character
 def Ruben():
     return "Ruben"
 
 ruben_character = "Orion"
 
 ruben_paragraphs = [
-    "Orion, a master cartographer, began drawing connections on the map. He had Marcus, Luna, Elara, and Draco examining every corner.",
-    "By the campfire, Orion narrated tales of ancient explorers to Marcus, Luna, Elara, and Draco.",
-    "Orion, using his skills, set the path ahead, guiding Marcus, Luna, Elara, and Draco."
+    f"{ruben_character}, a master cartographer, began drawing connections on the map. He had {xiao_character}, {sebastian_character}, {kelt_character}, and {sofia_character} examining every corner.",
+    f"By the campfire, {ruben_character} narrated tales of ancient explorers to {xiao_character}, {sebastian_character}, {kelt_character}, and {sofia_character}.",
+    f"{ruben_character}, using his skills, set the path ahead, guiding {xiao_character}, {sebastian_character}, {kelt_character}, and {sofia_character}."
 ]
