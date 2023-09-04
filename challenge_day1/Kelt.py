1b06b2ffe03ebd190bbf416939d6f56cd234d0af
kelt-uva <126686353+kelt-uva@users.noreply.github.com>
2023-09-04 13:55:22 +0200

    Kelt.py

--- challenge_day1/Kelt.py
+++ challenge_day1/Kelt.py
@@ -1,2 +1,10 @@
 def Kelt():
     return "Kelt"
+
+kelt_character = "Elara"
+
+kelt_paragraphs = [
+    "Elara's intuition told her there's more to the map than meets the eye. With Marcus, Luna, Orion, and Draco in tow, she set out to decipher it.",
+    "Elara, alongside Marcus, Luna, Orion, and Draco, gazed at the stars, seeking guidance.",
+    "As the sun broke, Elara, backed by Marcus, Luna, Orion, and Draco, found the first clue on their quest."
+]
