a7925a2b9c3369550255c050d36819ee999cea18
sofia-uva <126686357+sofia-uva@users.noreply.github.com>
2023-09-04 13:56:22 +0200

    Update sofia.py

--- challenge_day1/sofia.py
+++ challenge_day1/sofia.py
@@ -1,2 +1,10 @@
 def Sofia():
     return "Sofia"
+
+sofia_character = "Draco"
+
+sofia_paragraphs = [
+    "Draco, the youngest but most adventurous, was eager. Alongside Marcus, Luna, Elara, and Orion, he readied himself for the expedition.",
+    "Draco played a cheerful melody, lifting the spirits of Marcus, Luna, Elara, and Orion.",
+    "Determined, Draco took the lead, inspiring Marcus, Luna, Elara, and Orion to follow."
+]
