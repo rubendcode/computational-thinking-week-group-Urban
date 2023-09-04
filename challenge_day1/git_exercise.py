32b17dd29ef27f6461c4211419fd8e32db61d032
Quanpu Xiao <aoyamaxx@re-byods-146-50-199-238.wireless.uva.nl>
2023-09-04 13:28:27 +0200

    File changes

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,8 +1,10 @@
 from Xiao import Xiao
+from Sebastian import Sebastian
+from ruben import Ruben
 
 def naming():
-    members = [Xiao()]
-    members_str = ",\n".join(members)
-    return f"This is Team Urban.\nWe are:\n{members_str},\n"
+    members = [Xiao(), Sebastian(), Ruben()]
+    members_str = "\n".join(members)
+    return f"This is Team Urban.\nWe are:\n{members_str}"
 
 print(naming())
