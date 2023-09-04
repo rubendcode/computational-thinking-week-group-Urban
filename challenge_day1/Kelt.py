3b05f8eb3eaaba504b458dca57ef84e74ccd8b4c
kelt-uva <kelt.paehlig@student.uva.nl>
2023-09-04 14:29:18 +0200

    variable change

--- challenge_day1/Kelt.py
+++ challenge_day1/Kelt.py
@@ -4,7 +4,7 @@ def Kelt():
 kelt_character = "Elara"
 
 kelt_paragraphs = [
-    "Elara's intuition told her there's more to the map than meets the eye. With Marcus, Luna, Orion, and Draco in tow, she set out to decipher it.",
-    "Elara, alongside Marcus, Luna, Orion, and Draco, gazed at the stars, seeking guidance.",
-    "As the sun broke, Elara, backed by Marcus, Luna, Orion, and Draco, found the first clue on their quest."
+    f"{kelt_character}'s intuition told her there's more to the map than meets the eye. With Marcus, Luna, Orion, and Draco in tow, she set out to decipher it.",
+    f"{kelt_character}, alongside Marcus, Luna, Orion, and Draco, gazed at the stars, seeking guidance.",
+    f"As the sun broke, {kelt_character}, backed by Marcus, Luna, Orion, and Draco, found the first clue on their quest."
 ]
